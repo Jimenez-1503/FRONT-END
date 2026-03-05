498ce17eefc8069acc27e8d6d48eb71f787e0ad0
Jimenez-1503 <pedro.jimenez@aluno.senai.br>
2026-03-05 13:15:59 -0300

    alteracoes

--- correcao.js
+++ correcao.js
@@ -1,3 +1,13 @@
-// 1:
+let nome = prompt("Nome: ").trim();
+let sobrenome = prompt("Sobrenome: ").trim();
 
-let nome = prompt()
+let completo = nome + " " + sobrenome;
+
+// Nome completo em minúsculo
+alert(completo.toLowerCase());
+
+// Nome completo em maiúsculo
+alert(completo.toUpperCase());
+
+// Quantidade de caracteres
+alert(completo.length);
