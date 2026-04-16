d92bc60fefd96760ac8078eadd124815f4f3320c
Jimenez-1503 <pedro.jimenez@aluno.senai.br>
2026-04-16 08:45:59 -0300

    alteração aula 6

--- Aula-6/exemplos/src/scripts/aula6.js
+++ Aula-6/exemplos/src/scripts/aula6.js
@@ -63,15 +63,3 @@ cards.forEach((card) => {
     });
 });
 
-const inputQtd = document.querySelector("#qtd-lasanha");
-const precoTexto = document.querySelector("#preco-lasanha");
-
-if (inputQtd && precoTexto) {
-    inputQtd.addEventListener("input", () => {
-        const precoUnitario = 45.0;
-        const total = Number(inputQtd.value) * precoUnitario;
-        precoTexto.textContent = `R$ ${total.toFixed(2)}`;
-
-        
-    })
-}
